5f65f199f27bb5917851eb8ab2fdf4501f639f7b
Kovalev Dmitry <dima20000928@gmail.com>
2024-06-04 15:14:12 +0000

    Refactor Reading generic instances the same way as writing. Close inefficient parsing of input bytestring

--- ClickHaskell-generics/lib/ClickHaskell/Generics.hs
+++ ClickHaskell-generics/lib/ClickHaskell/Generics.hs
@@ -30,17 +30,14 @@ import ClickHaskell.Tables (CompiledColumn(..), InterpretableTable(..))
 
 
 -- GHC included
-import Data.ByteString         as BS (split, intercalate, StrictByteString)
-import Data.ByteString.Builder as BS (Builder, byteString)
-import Data.ByteString.Char8   as BS8 (pack)
-import Data.Data               (Proxy(..))
+import Data.ByteString         as BS (StrictByteString, drop)
+import Data.ByteString.Char8   as BS8 (span)
+import Data.ByteString.Builder as BS (Builder)
 import Data.Type.Bool          (If)
 import Data.Type.Equality      (type(==))
-import Data.Type.Ord           (type(>?), type(<=?))
-import Data.Word               (Word8)
 import Data.Kind               (Type, Constraint)
 import GHC.Generics            (K1(..), M1(..), type (:*:)(..), Rec0, D1, C1, S1, Meta(MetaSel), Generic (..))
-import GHC.TypeLits            (KnownSymbol, TypeError, Symbol, ErrorMessage(..), symbolVal)
+import GHC.TypeLits            (TypeError, Symbol, ErrorMessage(..))
 
 
 -- * Writing
@@ -124,26 +121,6 @@ type family ThereIsNoWriteRequiredColumns (columns :: [Type]) :: Constraint wher
       (ThereIsNoWriteRequiredColumns columns)
       (TypeError ('Text "Column " :<>: 'Text (GetColumnName column) :<>: 'Text " is required for insert but is missing"))
 
-type family
-  TakeColumn (name :: Symbol) (columns :: [Type]) :: (Type, [Type])
-  where
-  TakeColumn name columns = GoTakeColumn name columns '[]
-
-type family
-  GoTakeColumn name (columns :: [Type]) (acc :: [Type]) :: (Type, [Type])
-  where
-  GoTakeColumn name (column ': columns) acc = If (name == GetColumnName column) '(column, acc ++ columns) (GoTakeColumn name columns (column ': acc))
-  GoTakeColumn name '[]                 acc = TypeError
-    (    'Text "There is no column \"" :<>: 'Text name :<>: 'Text "\" in table"
-    :$$: 'Text "You can't insert this field"
-    )
-
-type family
-  (++) (list1 :: [Type]) (list2 :: [Type]) :: [Type]
-  where
-  (++) '[]            list = list
-  (++) (head ': tail) list = tail ++ (head ': list)
-
 
 
 
@@ -155,166 +132,89 @@ type family
 
 class
   ( InterpretableTable table
-  , GReadable (ValidatedTable table) (GetTableColumns table) (Rep record)
+  , GReadable (GetTableColumns table) (Rep record)
   ) =>
   ReadableFrom table record
   where
 
   default fromTsvLine :: (Generic record) => StrictByteString -> record
   fromTsvLine :: StrictByteString -> record
-  fromTsvLine = to . gFromTsvBs @(ValidatedTable table) @(GetTableColumns table)
+  fromTsvLine = to . gFromTsvBs @(GetTableColumns table)
 
   default readingColumns :: (Generic record) => Builder
   readingColumns :: Builder
-  readingColumns = gReadingColumns @(ValidatedTable table) @(GetTableColumns table) @(Rep record)
+  readingColumns = gReadingColumns @(GetTableColumns table) @(Rep record)
 
 
 class GReadable
-  (deivingState :: Maybe ErrorMessage)
   (columns :: [Type])
   f
   where
   gFromTsvBs :: StrictByteString -> f p
   gReadingColumns :: Builder
 
+instance {-# OVERLAPPING #-}
+  ( GReadable columns f
+  ) => GReadable columns (D1 c (C1 c2 f))
+  where
+  gFromTsvBs = M1 . M1 . gFromTsvBs @columns
+  gReadingColumns = gReadingColumns @columns @f
 
-instance
-  ( GReadable
-    ('Just
-      (    'Text "There is no column \"" :<>: 'Text columnName :<>: 'Text "\" in table."
-      :$$: 'Text "You can't select this field"
-      )
+
+instance {-# OVERLAPPING #-}
+  ( GReadable columns (left1 :*: (left2 :*: right))
+  ) => GReadable columns ((left1 :*: left2) :*: right)
+  where
+  gFromTsvBs bs =
+    let (left1 :*: (left2 :*: right)) = gFromTsvBs @columns bs
+    in ((left1 :*: left2) :*: right)
+  gReadingColumns = gReadingColumns @columns @(left1 :*: (left2 :*: right))
+
+instance {-# OVERLAPPING #-}
+  ( CompiledColumn column
+  , '(column, restColumns) ~ TakeColumn selectorName columns
+  , FromChType (GetColumnType column) inputType
+  , Deserializable (GetColumnType column)
+  , GReadable restColumns right
+  ) => GReadable columns (S1 (MetaSel (Just selectorName) a b f) (Rec0 inputType) :*: right)
+  where
+  gFromTsvBs bs = 
+    let (beforeTab, afterTab) = BS8.span (/= '\t') bs 
+    in 
+    (M1 . K1 . fromChType @(GetColumnType column) . deserialize $ beforeTab) :*: gFromTsvBs @restColumns @right (BS.drop 1 afterTab)
+  gReadingColumns = renderColumnName @column <> ", " <> gReadingColumns @restColumns @right
+
+instance {-# OVERLAPPING #-}
+  ( CompiledColumn column
+  , '(column, restColumns) ~ TakeColumn selectorName columns
+  , Deserializable (GetColumnType column)
+  , FromChType (GetColumnType column) inputType
+  ) => GReadable columns ((S1 (MetaSel (Just selectorName) a b f)) (Rec0 inputType))
+  where
+  gFromTsvBs = M1 . K1 . fromChType @(GetColumnType column) . deserialize
+  gReadingColumns = renderColumnName @column
+
+
+
+
+-- * Reusable in both scenarios
+
+type family
+  TakeColumn (name :: Symbol) (columns :: [Type]) :: (Type, [Type])
+  where
+  TakeColumn name columns = GoTakeColumn name columns '[]
+
+type family
+  GoTakeColumn name (columns :: [Type]) (acc :: [Type]) :: (Type, [Type])
+  where
+  GoTakeColumn name (column ': columns) acc = If (name == GetColumnName column) '(column, acc ++ columns) (GoTakeColumn name columns (column ': acc))
+  GoTakeColumn name '[]                 acc = TypeError
+    (    'Text "There is no column \"" :<>: 'Text name :<>: 'Text "\" in table"
+    :$$: 'Text "You can't use this field"
     )
-    '[]
-    (S1 (MetaSel (Just columnName) a b f) k)
-  ) => GReadable
-    'Nothing
-    '[]
-    (S1 (MetaSel (Just columnName) a b f) k)
+
+type family
+  (++) (list1 :: [Type]) (list2 :: [Type]) :: [Type]
   where
-  gFromTsvBs _ = error "Unreachable"
-  gReadingColumns = error "Unreachable"
-
-
-instance
-  ( TypeError errorMsg
-  ) => GReadable ('Just errorMsg) columns genericRep
-  where
-  gFromTsvBs _ = error "Unreachable"
-  gReadingColumns = error "Unreachable"
-
-
-instance {-# OVERLAPPING #-}
-  ( GReadable 'Nothing columns f
-  ) => GReadable 'Nothing columns (D1 c (C1 c2 f))
-  where
-  gFromTsvBs = M1 . M1 . gFromTsvBs @'Nothing @columns
-  gReadingColumns = gReadingColumns @'Nothing @columns @f
-
-
-instance {-# OVERLAPPING #-}
-  ( leftCenterTreeElement  ~ GetGenericProductLastSelector left
-  , rightCenterTreeElement ~ GetGenericProductHeadSelector right
-  , derivingState ~ FirstJustOrNothing
-    '[ GetGenericProductHeadSelector left `AssumePlacedBefore` leftCenterTreeElement
-     , leftCenterTreeElement              `AssumePlacedBefore` rightCenterTreeElement
-     , rightCenterTreeElement             `AssumePlacedBefore` GetGenericProductLastSelector right
-     ]
-  , '(firstColumnsPart, secondColumnsPart) ~ SpanByColumnName rightCenterTreeElement columns
-  , GReadable derivingState firstColumnsPart left
-  , GReadable derivingState secondColumnsPart right
-  ) => GReadable 'Nothing columns (left :*: right)
-  where
-  gFromTsvBs bs = {- [ClickHaskell.DataDsl.ToDo.1]: optimize line deconstruction -}
-    let byteStrings = tabSymbol `BS.split` bs
-        (leftWords, rightWords) = splitAt (length byteStrings `div` 2) byteStrings
-    in  gFromTsvBs @derivingState @firstColumnsPart (BS.intercalate "\t" leftWords)
-    :*: gFromTsvBs @derivingState @secondColumnsPart (BS.intercalate "\t" rightWords)
-    where
-    tabSymbol :: Word8
-    tabSymbol = 9
-  gReadingColumns
-    =          gReadingColumns @derivingState @firstColumnsPart @left
-    <> ", " <> gReadingColumns @derivingState @secondColumnsPart @right
-
-
-instance {-# OVERLAPPING #-}
-  ( Deserializable (GetColumnType column)
-  , FromChType (GetColumnType column) outputType
-  , KnownSymbol columnName
-  ) => GReadable
-    'Nothing
-    (column ': xs)
-    (S1 (MetaSel (Just columnName) a b f) (K1 i outputType))
-  where
-  gFromTsvBs = M1 . K1 . fromChType @(GetColumnType column) @outputType . deserialize
-  gReadingColumns = (BS.byteString . BS8.pack . symbolVal) (Proxy @columnName)
-
-
-
-
-type family (sym1 :: Symbol) `AssumePlacedBefore` (sym2 :: Symbol) :: Maybe ErrorMessage
-  where
-  sym1 `AssumePlacedBefore` sym2 =
-    If (sym1 >? sym2)
-      ('Just
-      (    'Text "Record fields should be sorted alphabetically."
-      :$$: 'Text "But field \"" :<>: 'Text sym2 :<>: 'Text "\" placed before \"" :<>: 'Text sym1 :<>: 'Text "\""
-      )
-      )
-      'Nothing
-
-type family GetGenericProductHeadSelector (f :: k -> Type) :: Symbol where
-  GetGenericProductHeadSelector (c :*: c2) = GetGenericProductHeadSelector c
-  GetGenericProductHeadSelector (D1 _ f)   = GetGenericProductHeadSelector f
-  GetGenericProductHeadSelector (C1 _ f)   = GetGenericProductHeadSelector f
-  GetGenericProductHeadSelector (S1 (MetaSel (Just sel) _ _ _) _) = sel
-
-type family GetGenericProductLastSelector (f :: k -> Type) :: Symbol where
-  GetGenericProductLastSelector (c :*: c2) = GetGenericProductLastSelector c2
-  GetGenericProductLastSelector (D1 _ f)   = GetGenericProductLastSelector f
-  GetGenericProductLastSelector (C1 _ f)   = GetGenericProductLastSelector f
-  GetGenericProductLastSelector (S1 (MetaSel (Just sel) _ _ _) _) = sel
-
-type family SpanByColumnName
-  (name :: Symbol)
-  (columns :: [Type])
-  ::
-  ([Type], [Type])
-  where
-  SpanByColumnName name columns = GoSpanByColumnName name '(columns, '[])
-
-type family GoSpanByColumnName
-  (name :: Symbol)
-  (acc :: ([Type], [Type]))
-  ::
-  ([Type], [Type])
-  where
-  GoSpanByColumnName name '( '[], acc2) = '(Reverse acc2, '[])
-  GoSpanByColumnName name '(column ': columns, acc2) =
-    If (name <=? GetColumnName column)
-      '(Reverse acc2, column ': columns)
-      (GoSpanByColumnName name '(columns, column ': acc2))
-
-type family Reverse (b :: [a]) :: [a] where Reverse list = GoReverse list '[]
-type family GoReverse (list :: [a]) (acc :: [a])
-  where
-  GoReverse '[]       acc = acc
-  GoReverse (x ': xs) acc = GoReverse xs (x ': acc)
-
--- | Type family usefull when you have a several posible errors and need to return first one.
--- 
--- @
--- type NotAnError = 'Nothng
--- type Error1     = 'Just ('Text "error1")
--- type Error2     = 'Just ('Text "error2")
---
--- type FirstJustOrNothing '[NotAnError, Error2] = 'Just ('Text "error2")
--- type FirstJustOrNothing '[Error1,     Error2] = 'Just ('Text "error1")
--- type FirstJustOrNothing '[NotAnError]         = 'Nothing
--- @
-type family FirstJustOrNothing (a :: [Maybe ErrorMessage]) :: Maybe ErrorMessage
-  where
-  FirstJustOrNothing '[] = 'Nothing
-  FirstJustOrNothing ('Nothing ': xs) = FirstJustOrNothing xs
-  FirstJustOrNothing ('Just txt ': xs) = 'Just txt
+  (++) '[]            list = list
+  (++) (head ': tail) list = tail ++ (head ': list)
