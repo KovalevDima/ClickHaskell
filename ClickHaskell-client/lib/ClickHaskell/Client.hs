f36c505a64bcc5a3a45528f3dbc18e769132a115
KovalevDima <dima20000928@gmail.com>
2024-06-30 04:16:56 +0300

    Move unsafeInterleaveIO

--- ClickHaskell-client/lib/ClickHaskell/Client.hs
+++ ClickHaskell-client/lib/ClickHaskell/Client.hs
@@ -149,9 +149,9 @@ instance ImpliesClickHouseHttp H.Request (H.Response BodyReader) where
   injectReadingToResponse decoder = fmap (decoder . mconcat) . brConsume . responseBody
 
   injectWritingToRequest query dataList encoder request = request{
-    requestBody = RequestBodyStreamChunked $ \np -> do
+    requestBody = RequestBodyStreamChunked $ unsafeInterleaveIO . \np -> do
       writingData <- newIORef . BL.toChunks . toLazyByteString . mconcat . (query:) . map encoder $ dataList
-      np . unsafeInterleaveIO $ do
+      np $ do
         bss <- readIORef writingData
         case bss of
           [] -> return BS.empty
