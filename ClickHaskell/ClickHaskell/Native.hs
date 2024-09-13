d149b013f561b890a8e7cc1ef4787feed87c8cc2
KovalevDima <dima20000928@gmail.com>
2024-09-13 03:29:05 +0300

    Fix current status: refactor serialization of hello packet + WIP query packet

--- ClickHaskell/ClickHaskell/Native.hs
+++ ClickHaskell/ClickHaskell/Native.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings, DeriveAnyClass, NamedFieldPuns, NumericUnderscores #-} 
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, DeriveAnyClass, NamedFieldPuns, NumericUnderscores #-}
 module ClickHaskell.Native where
 
 -- Internal dependencies
 import ClickHaskell.DbTypes
+import Paths_ClickHaskell (version)
 
 -- GHC included
 import Control.Exception (Exception, SomeException, bracketOnError, catch, finally, throw)
-import Data.ByteString.Builder (Builder, toLazyByteString, word8)
-import Data.ByteString.Char8 as BS8 (toStrict)
+import Data.ByteString as BS (length)
+import Data.ByteString.Builder as BS (Builder, byteString, toLazyByteString, word8)
+import Data.ByteString.Char8 as BS8 (ByteString, toStrict)
 import Data.Maybe (fromMaybe, listToMaybe)
-import Data.Text as Text (Text, unpack)
+import Data.Text as Text (Text)
+import Data.Version (Version (..))
 import Data.Word (Word16, Word32, Word64, Word8)
 import Foreign (Bits (..), Ptr)
 import GHC.Generics (Generic)
@@ -18,10 +21,9 @@ import System.Timeout (timeout)
 
 -- External
 import Network.Socket.ByteString (recv, send)
-import Data.Text.Encoding (encodeUtf8Builder)
 
 openNativeConnection :: ChCredential -> IO (Socket, SockAddr)
-openNativeConnection MkChCredential{chHost} = do
+openNativeConnection MkChCredential{chHost, chPort} = do
   AddrInfo
     { addrFamily
     , addrSocketType
@@ -33,8 +35,8 @@ openNativeConnection MkChCredential{chHost} = do
     <$>
     getAddrInfo
       (Just defaultHints{addrFlags = [AI_ADDRCONFIG], addrSocketType = Stream})
-      (Just $ Text.unpack chHost)
-      (Just "tcp")
+      (Just chHost)
+      (Just chPort)
 
   (fromMaybe (throw EstablishTimeout) <$>) . timeout 3_000_000 $
     bracketOnError
@@ -59,7 +61,8 @@ devCredential = MkChCredential
   { chLogin = "default"
   , chPass = ""
   , chDatabase = "default"
-  , chHost = "localhost:9000"
+  , chHost = "localhost"
+  , chPort = "9000"
   }
 
 
@@ -67,7 +70,8 @@ data ChCredential = MkChCredential
   { chLogin    :: Text
   , chPass     :: Text
   , chDatabase :: Text
-  , chHost     :: Text
+  , chHost     :: HostName
+  , chPort     :: ServiceName
   }
   deriving (Generic, Show, Eq)
 
@@ -85,13 +89,31 @@ class Serializable chType
   where
   serialize :: chType -> Builder
 
+instance Serializable ChUInt8 where
+  serialize = vlq128 . fromChType @ChUInt8 @Word8
+
+instance Serializable ChUInt16 where
+  serialize = vlq128 . fromChType @ChUInt16 @Word16
+
+instance Serializable ChUInt32 where
+  serialize = vlq128 . fromChType @ChUInt32 @Word32
+
 instance Serializable ChUInt64 where
   serialize = vlq128 . fromChType @ChUInt64 @Word64
+
+instance Serializable ChUInt128 where
+  serialize = vlq128 . fromChType @ChUInt128 @Word128
+
+instance Serializable ChString where
+  serialize str
+    =  (serialize @ChUInt64 . fromIntegral . BS.length . fromChType @_ @ByteString) str
+    <> (BS.byteString . fromChType @_ @ByteString) str
 
 dev :: IO ()
 dev = do
   (sock, _sockAddr) <- openNativeConnection devCredential
-  sendHello devCredential sock
+  sendHelloPacket devCredential sock
+  sendQueryPacket sock "SELECT 5"
 
 
 {-# SPECIALIZE vlq128 :: Word8 -> Builder #-}
@@ -106,18 +128,43 @@ vlq128 = go
     | otherwise = word8 (0x80 .&. (fromIntegral i .|. 0x7F)) <> go (i `unsafeShiftR` 7)
 
 
-sendHello :: ChCredential -> Socket -> IO ()
-sendHello MkChCredential{chDatabase, chLogin, chPass} sock = do
+
+sendHelloPacket :: ChCredential -> Socket -> IO ()
+sendHelloPacket MkChCredential{chDatabase, chLogin, chPass} sock = do
   _sentSize <- send sock
     (toStrict . toLazyByteString . mconcat $
-      [ vlq128 @Word8 0            -- Hello packet code
-      , vlq128 @Word8 5, "hello"   -- Client name: "Hello"
-      , vlq128 @Word8 0            -- Major version: 0
-      , vlq128 @Word8 1            -- Minor version: 0
-      , vlq128 @Word16 55_255      -- Protocol version
-      , vlq128 @Word8 7, encodeUtf8Builder chDatabase -- Database name: "default"
-      , vlq128 @Word8 7, encodeUtf8Builder chLogin -- User name: "default"
-      , vlq128 @Word8 0, encodeUtf8Builder chPass        -- Password: ""
+      -- Hello packet code
+      [ serialize @ChUInt8 0
+      -- Client name
+      , serialize @ChString "ClickHaskell"
+      -- Major version
+      , serialize @ChUInt16 (fromIntegral $ versionBranch version !! 0)
+      -- Minor version
+      , serialize @ChUInt16 (fromIntegral $ versionBranch version !! 1)
+      -- Protocol version
+      , serialize @ChUInt16 55_255
+      -- Database name
+      , serialize @ChString (toChType chDatabase)
+      -- User name
+      , serialize @ChString (toChType chLogin)
+      -- Password
+      , serialize @ChString (toChType chPass)
+      ]
+    )
+  bs <- recv sock 4096
+  print bs
+
+sendQueryPacket :: Socket -> ChString -> IO ()
+sendQueryPacket sock query = do
+  _sentSize <- send sock
+    (toStrict . toLazyByteString . mconcat $
+      [ serialize @ChString "someId", serialize @ChString ""
+      , serialize @ChString ""
+      , serialize @ChString ""
+      , serialize @ChUInt8 2
+      , serialize @ChUInt8 0
+      , serialize @ChString query
+      , serialize @ChString ""
       ]
     )
   bs <- recv sock 4096
