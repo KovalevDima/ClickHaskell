acd0c4f72eb0132b12ef0f8255c099b8cd25f649
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2024-12-15 01:43:42 +0300

    Refactor parameters (#94)
    * Refactor parameters

--- distribution/ClickHaskell/NativeProtocol.hs
+++ distribution/ClickHaskell/NativeProtocol.hs
@@ -29,9 +29,10 @@ import Data.Binary.Get.Internal (readN)
 import Data.Binary.Put
 import Data.Bits
 import Data.ByteString as BS (length, take)
-import Data.ByteString.Builder (Builder, stringUtf8, word8)
+import Data.ByteString.Builder (Builder, stringUtf8, word8, byteString)
+import Data.ByteString.Char8 as BS8 (pack)
 import Data.Coerce (coerce)
-import Data.Kind (Type)
+import Data.Kind (Type, Constraint)
 import Data.Typeable (Proxy (..))
 import Debug.Trace (traceShowId)
 import GHC.Generics
@@ -961,6 +962,68 @@ instance {-# OVERLAPPING #-}
     -- serialization is not custom
     <> afterRevision @DBMS_MIN_REVISION_WITH_CUSTOM_SERIALIZATION rev (serialize @ChUInt8 rev 0)
     <> undefined column
+
+
+
+
+
+
+
+
+-- * Parameters
+
+type family KnownParameter param
+  where
+  KnownParameter (Parameter name parType) = (KnownSymbol name, ToQueryPart parType)
+
+data Parameter (name :: Symbol) (chType :: Type) = MkParamater chType
+
+data Parameters parameters where
+  NoParameters :: Parameters '[]
+  AddParameter
+    :: KnownParameter (Parameter name chType)
+    => Parameter name chType
+    -> Parameters parameters
+    -> Parameters (Parameter name chType ': parameters)
+
+-- >>> import ClickHaskell.DbTypes
+{- |
+>>> parameters (parameter @"a3" @ChString ("a3Val" :: String) . parameter @"a2" @ChString ("a2Val" :: String))
+"(a3='a3Val', a2='a2Val')"
+-}
+viewParameters :: (Parameters '[] -> Parameters passedParameters) -> Builder
+viewParameters interpreter = "(" <> renderParameters (interpreter NoParameters) <> ")"
+
+renderParameters :: Parameters params -> Builder
+renderParameters NoParameters                      = ""
+renderParameters (AddParameter param NoParameters) = renderParameter param
+renderParameters (AddParameter param moreParams)   = renderParameter param <> ", " <> renderParameters moreParams
+
+
+parameter
+  :: forall name chType parameters userType
+  . (ToChType chType userType, KnownParameter (Parameter name chType))
+  => userType -> Parameters parameters -> Parameters (Parameter name chType ': parameters)
+parameter val = AddParameter (MkParamater $ toChType val)
+
+renderParameter :: forall name chType . KnownParameter (Parameter name chType) => Parameter name chType -> Builder
+renderParameter (MkParamater chType) = (byteString . BS8.pack . symbolVal @name) Proxy <> "=" <> toQueryPart chType
+
+type family CheckParameters (required :: [Type]) (passed :: [Type]) :: Constraint
+  where
+  CheckParameters required passed = GoCheckParameters required passed '[] 
+
+type family GoCheckParameters required passed acc :: Constraint
+  where
+  GoCheckParameters '[] '[] '[] = ()
+  GoCheckParameters (Parameter name _ ': _) '[] '[] = TypeError ('Text "Missing parameter \"" :<>: 'Text name :<>: 'Text "\".")
+  GoCheckParameters '[] (p ': _) _ = TypeError ('Text "More parameters passed than used in the view")
+  GoCheckParameters '[] '[] (p ': _) = TypeError ('Text "More parameters passed than used in the view")
+  GoCheckParameters (Parameter name1 _ ': ps) '[] (Parameter name2 _ ': ps') = TypeError ('Text "Missing  \"" :<>: 'Text name1 :<>: 'Text "\" in passed parameters")
+  GoCheckParameters (p ': ps) '[] (p' ': ps') = GoCheckParameters (p ': ps) (p' ': ps') '[]
+  GoCheckParameters (Parameter name1 _ ': ps) (Parameter name1 _ ': ps') acc = (GoCheckParameters ps ps' acc)
+  GoCheckParameters (Parameter name1 chType1 ': ps) (Parameter name2 chType2 ': ps') acc
+    = (GoCheckParameters (Parameter name1 chType1 ': ps) ps' (Parameter name2 chType2 ': acc))
 
 
 
