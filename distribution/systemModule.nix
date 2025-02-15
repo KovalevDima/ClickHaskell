f786133b848398982624acb452de87fb5fc67b9c
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-02-15 23:59:33 +0300

    Prepare system module

--- distribution/systemModule.nix
+++ distribution/systemModule.nix
@@ -1,13 +1,28 @@
 { config
+, self
 , lib
-, pkgs
 , ...
 }:
-
+let
+  path = config.ClickHaskell.path;
+  domain = config.ClickHaskell.domain;
+in
 {
-  options.ClickHaskell = lib.mkOption {
-    type = lib.types.passwdEntry lib.types.path;
-    default = "/var/lib/ClickHaskell";
+  options.ClickHaskell = {
+    path = lib.mkOption {
+      type = lib.types.passwdEntry lib.types.path;
+      default = "/var/lib/ClickHaskell";
+      description = "Filepath to state directory";
+    };
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = "clickhaskell.dev";
+      description = "Domain name for ClickHaskell infrastructure";
+    };
+    pagePackage = lib.mkOptions {
+      type = lib.types.package;
+      default = self.packages.x86_64-linux."documentation";
+    };
   };
   config = {
     users = {
@@ -15,6 +30,17 @@
       users.ClickHaskell = {
         homeMode = "755";
         isSystemUser = true;
+      };
+    };
+
+    security.acme = {
+      acceptTerms = true;
+      certs = {
+        "${domain}" = {
+          group = "ClickHaskell";
+          email = "letsencrypt@${domain}";
+          directory = "${path}/certs";
+        };
       };
     };
   };
