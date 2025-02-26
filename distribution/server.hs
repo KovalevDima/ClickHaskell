be56a6739afb5a749bf0c2f22a3758fb0a50235b
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2025-02-26 23:02:30 +0300

    Add ClickHouse to system module and write stats (#156)
    * Add ClickHouse to system module and write stats

--- distribution/server.hs
+++ distribution/server.hs
@@ -1,64 +1,159 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DerivingStrategies #-}
 
+import ClickHaskell
+  ( ChString, ChUInt32, ChDateTime
+  , Column, Table
+  , WritableInto, insertInto
+  , Connection, openNativeConnection, defaultCredentials
+  )
 import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async, waitAnyCancel)
+import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TBQueue
-import Control.Monad (filterM, forM, forever)
-import Data.ByteString.Char8 as BS8 (unpack)
-import Data.ByteString.Lazy as B hiding (map)
-import Data.HashMap.Strict as HM hiding (map)
+import Control.Exception (SomeException, catch)
+import Control.Monad (filterM, forM, forever, void)
+import Data.ByteString (StrictByteString)
+import Data.ByteString.Char8 as BS8 (pack, unpack)
+import Data.ByteString.Lazy as B (LazyByteString, readFile)
+import Data.HashMap.Strict as HM (HashMap, empty, fromList, lookup, unions)
 import Data.Text as T (pack)
+import Data.Time (getCurrentTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import Data.Word (Word32)
+import GHC.Generics (Generic)
+import Net.IPv4 (decodeUtf8, getIPv4)
 import Network.HTTP.Types (status200, status404)
 import Network.HTTP.Types.Header (hContentType)
 import Network.Mime (MimeType, defaultMimeLookup)
-import Network.Socket (Family (..), SockAddr (..), SocketType (..), bind, socket, listen, maxListenQueue)
+import Network.Socket (Family (..), SockAddr (..), SocketType (..), bind, listen, maxListenQueue, socket)
 import Network.Wai (Request (..), Response, ResponseReceived, responseLBS)
-import Network.Wai.Handler.Warp (defaultSettings, runSettings, runSettingsSocket, setPort)
+import Network.Wai.Handler.Warp (Port, defaultSettings, runSettings, runSettingsSocket, setPort)
 import System.Directory (doesDirectoryExist, listDirectory, withCurrentDirectory)
 import System.Environment (lookupEnv)
 import System.FilePath (dropFileName, dropTrailingPathSeparator, normalise, takeFileName, (</>))
 
+{-
+```
+CREATE TABLE default.ClickHaskellStats
+(
+    `time` DateTime,
+    `path` LowCardinality(String),
+    `remoteAddr` UInt32
+)
+ENGINE = MergeTree
+PARTITION BY path
+ORDER BY path
+SETTINGS index_granularity = 8192;
+```
+-}
 
 main :: IO ()
 main = do
   mSocketPath  <- lookupEnv "CLICKHASKELL_PAGE_SOCKET_PATH"
   mStaticFiles <- lookupEnv "CLICKHASKELL_STATIC_FILES_DIR"
 
-  staticFiles <- maybe (pure HM.empty) (flip withCurrentDirectory (listFilesWithContents ".")) mStaticFiles
+  staticFiles   <- maybe (pure HM.empty) (flip withCurrentDirectory (listFilesWithContents ".")) mStaticFiles
   docsStatQueue <- newTBQueueIO 100_000
+  clickHouse    <- openNativeConnection defaultCredentials
+
+  serverTask <- async $ runServer MkServerState{..} mSocketPath
+  databaseWriter <- async $
+    forever $ do
+      catch
+        @SomeException
+        (writeStats MkDocsStatisticsState{..})
+        print
+      threadDelay 5_000_000
+  void $ waitAnyCancel [serverTask, databaseWriter]
+
+data DocsStatistics = MkDocsStatistics
+  { time       :: Word32
+  , path       :: StrictByteString
+  , remoteAddr :: Word32
+  }
+  deriving stock (Generic)
+  deriving anyclass (WritableInto DocsStatTable)
+
+type DocsStatTable = 
+  Table 
+    "ClickHaskellStats"
+   '[ Column "time" (ChDateTime "")
+    , Column "path" ChString
+    , Column "remoteAddr" ChUInt32
+    ]
+
+
+
+
+{-
+
+Stats handler
+
+-}
+
+data DocsStatisticsState = MkDocsStatisticsState
+  { docsStatQueue :: TBQueue DocsStatistics
+  , clickHouse :: Connection
+  }
+
+writeStats :: DocsStatisticsState -> IO ()
+writeStats MkDocsStatisticsState{..} = do
+  dataToWrite <- (atomically . flushTBQueue) docsStatQueue
+  insertInto @DocsStatTable clickHouse dataToWrite
+
+
+
+
+{-
+
+Web application core logic
+
+-}
+
+data ServerState = MkServerState
+  { staticFiles :: HashMap FilePath (MimeType, LazyByteString)
+  , docsStatQueue :: TBQueue DocsStatistics
+  , clickHouse :: Connection
+  }
+
+runServer :: ServerState -> Maybe FilePath -> IO ()
+runServer serverState mSocketPath = do
   case SockAddrUnix <$> mSocketPath of
-    Nothing ->
-      runSettings
-        (setPort 3000 $ defaultSettings)
-        (app MkServerState{..})
+    Nothing -> do
+      let port = 3000 :: Port
+      putStrLn $ "Starting server on http://localhost:" <> show port
+      runSettings (setPort port defaultSettings) (app serverState)
     Just sockAddr -> do
       sock <- socket AF_UNIX Stream 0
       putStrLn $ "Starting server on UNIX socket: " ++ (show sockAddr)
       bind sock sockAddr
       listen sock maxListenQueue
-      runSettingsSocket defaultSettings sock (app MkServerState{..})
-
-  forever $ threadDelay 60_000_000
-
-data DocsStat
-
-
-data ServerState = MkServerState
-  { staticFiles :: HashMap FilePath (MimeType, LazyByteString)
-  , docsStatQueue :: TBQueue DocsStat
-  }
+      runSettingsSocket defaultSettings sock (app serverState)
 
 
 app :: ServerState -> Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
-app MkServerState{staticFiles} req f = do
-  let path = dropIndexHtml . BS8.unpack $ rawPathInfo req
+app MkServerState{staticFiles, docsStatQueue} req f = do
+  let path = (dropIndexHtml . BS8.unpack . rawPathInfo) req
+      clientIp4 = maybe 0 getIPv4 (decodeUtf8 =<< Prelude.lookup "X-Real-IP" (requestHeaders req))
+  time <- floor . utcTimeToPOSIXSeconds <$> getCurrentTime
   case HM.lookup path staticFiles of
     Nothing -> f (responseLBS status404 [("Content-Type", "text/plain")] "404 - Not Found")
     Just (mimeType, content) -> do
+      (atomically . writeTBQueue docsStatQueue)
+        MkDocsStatistics
+          { time
+          , path       = BS8.pack path
+          , remoteAddr = clientIp4
+          }
       f (responseLBS status200 [(hContentType, mimeType)] content)
 
 
-listFilesWithContents :: FilePath -> IO (HashMap FilePath (MimeType, ByteString))
+listFilesWithContents :: FilePath -> IO (HashMap FilePath (MimeType, LazyByteString))
 listFilesWithContents dir = do
   entries <- listDirectory dir
   let paths = map (dir </>) entries
