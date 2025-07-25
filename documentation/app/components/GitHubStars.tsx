68493ff8c84e6e90debb324659811be00a92ad3f
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-07-25 05:53:21 +0300

    Update git start color

--- documentation/app/components/GitHubStars.tsx
+++ documentation/app/components/GitHubStars.tsx
@@ -22,7 +22,7 @@ function GitHubStars() {
   <>
     <span>{stars}</span>
     <svg aria-hidden="true" height="18" width="18" viewBox="0 0 16 16" version="1.1" data-view-component="true">
-      <g fill="var(--chart-1)">
+      <g fill="rgb(227, 179, 65)">
         <path
           d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z">
         </path>
