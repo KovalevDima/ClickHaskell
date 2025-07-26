a61c5143c39780012c8560bd470069fef01e00e0
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-07-26 15:15:39 +0300

    Separate visits chart

--- documentation/app/routes/_index.tsx
+++ documentation/app/routes/_index.tsx
@@ -1,37 +1,18 @@
-import { Link, Outlet } from 'react-router'
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
-
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-]
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "var(--chart-1)",
-  },
-} satisfies ChartConfig
-
+import { Card, CardContent } from '@/components/ui/card'
+import VisitsChart from '~/components/Visits'
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-y-3">
       <Card className="p-6 ">
-          <h1>Efficiency</h1>
-          <p>
-            ClickHaskell is designed to:
-          </p>
-          <ul>
-            <li>Handle millions of rows of data in constant memory</li>
-            <li>Efficiently utilize Haskell concurrent runtime</li>
-          </ul>
+        <h1>Efficiency</h1>
+        <p>
+          ClickHaskell is designed to:
+        </p>
+        <ul>
+          <li>Handle millions of rows of data in constant memory</li>
+          <li>Efficiently utilize Haskell concurrent runtime</li>
+        </ul>
       </Card>
 
       <div className="flex flex-row gap-3">
@@ -43,23 +24,7 @@ export default function HomePage() {
         </Card>
         <Card className="min-w-100">
           <CardContent>
-            <ChartContainer config={chartConfig}>
-              <BarChart accessibilityLayer data={chartData}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="month"
-                  tickLine={false}
-                  tickMargin={10}
-                  axisLine={false}
-                  tickFormatter={(value) => value.slice(0, 3)}
-                />
-                <ChartTooltip
-                  cursor={false}
-                  content={<ChartTooltipContent hideLabel />}
-                />
-                <Bar dataKey="desktop" fill="var(--color-desktop)" radius={0} />
-              </BarChart>
-            </ChartContainer>
+            <VisitsChart />
           </CardContent>
         </Card>
       </div>
