aa5b37a9f311d0efad0726e67bb9ea6f7234ef0f
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2025-08-19 02:48:17 +0300

    Add pings performance test (#351)

--- contribution/project.nix
+++ contribution/project.nix
@@ -9,6 +9,7 @@
     ClickHaskell = {libraryProfiling = true; haddock = true;};
     prof-1bil-stream = {libraryProfiling = true; executableProfiling = true;};
     prof-simple = {libraryProfiling = true; executableProfiling = true;};
+    prof-pings = {libraryProfiling = true; executableProfiling = true;};
   };
   defaults =  {
     devShell.tools = hp: with hp; {
