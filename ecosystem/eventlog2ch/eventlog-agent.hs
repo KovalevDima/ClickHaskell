a10376c2b9541ce229c65d95223a15248f7db4ef
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-09-28 22:05:57 +0300

    Revert user binary messages

--- ecosystem/eventlog2ch/eventlog-agent.hs
+++ ecosystem/eventlog2ch/eventlog-agent.hs
@@ -338,7 +338,7 @@ eventToName evSpec = case evSpec of
   HeapProfSampleString{}      -> "HeapProfSampleString"
   ProfSampleCostCentre{}      -> "ProfSampleCostCentre"
   ProfBegin{}                 -> "ProfBegin"
-  UserBinaryMessage msg       -> "UserBinaryMessage"
+  UserBinaryMessage{}         -> "UserBinaryMessage"
   ConcMarkBegin{}             -> "ConcMarkBegin"
   ConcMarkEnd{}               -> "ConcMarkEnd"
   ConcSyncBegin{}             -> "ConcSyncBegin"
