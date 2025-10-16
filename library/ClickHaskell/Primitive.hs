160c01a902fb3bcc55762f23bda3e40795c3df6e
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2025-10-16 11:41:49 +0300

    Fix Array deserialization order (#378)
    * Fix Array deserialization order

--- library/ClickHaskell/Primitive.hs
+++ library/ClickHaskell/Primitive.hs
@@ -46,12 +46,11 @@ class Serializable chType
 
 {-# INLINE replicateGet #-}
 replicateGet :: Serializable chType => ProtocolRevision -> UVarInt -> Get [chType]
-replicateGet rev cnt0 = loopGet cnt0 []
+replicateGet rev cnt0 = loopGet cnt0
   where
-  loopGet 0 acc = pure acc
-  loopGet n acc = do
-    !x <- deserialize rev
-    loopGet (n - 1) (x : acc)
+  loopGet cnt
+    | cnt == 0  = pure []
+    | otherwise = liftA2 (:) (deserialize rev) (loopGet (cnt - 1))
 
 instance Serializable prim => Serializable [prim] where
   serialize rev list
