466514592adbb18e755212b907b9e758ae122372
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2025-10-26 22:30:20 +0300

    Support settings (#387)
    * Support settings

--- library/ClickHaskell/Packets/Settings.hs
+++ library/ClickHaskell/Packets/Settings.hs
@@ -4,7 +4,14 @@ module ClickHaskell.Packets.Settings where
 import ClickHaskell.Primitive
 
 -- GHC
-import GHC.Generics
+import Control.Applicative (liftA2)
+import Data.Binary.Builder (Builder)
+import Data.Binary.Get (Get, lookAhead, skip)
+import Data.ByteString as BS (null)
+import Data.Kind (Type)
+import Data.Typeable (Proxy (..))
+import GHC.TypeLits (KnownSymbol, Symbol, symbolVal)
+import Prelude hiding (liftA2)
 
 -- * Server settings
 
@@ -14,26 +21,40 @@ addSetting :: DbSetting -> DbSettings -> DbSettings
 addSetting setting (MkDbSettings list) = MkDbSettings (setting : list)
 
 data DbSetting = MkDbSetting
-  { setting :: ChString
-  , flags   :: Flags `SinceRevision` DBMS_MIN_REVISION_WITH_SETTINGS_SERIALIZED_AS_STRINGS
-  , value   :: ChString
+  { setting    :: ChString 
+  , flags      :: Flags `SinceRevision` DBMS_MIN_REVISION_WITH_SETTINGS_SERIALIZED_AS_STRINGS
+  , value      :: SettingType
   }
-  deriving (Generic, Serializable)
+
+instance Serializable DbSetting where
+  deserialize rev = do
+    setting <- deserialize @ChString rev
+    flags <- deserialize @(Flags `SinceRevision` DBMS_MIN_REVISION_WITH_SETTINGS_SERIALIZED_AS_STRINGS) rev
+    case lookupSetting setting of
+      Nothing -> fail ("Unsupported setting " <> show setting)
+      Just MkSettingSerializer{deserializer} -> do
+        value <- deserializer rev
+        pure $ MkDbSetting{..} 
+  serialize rev MkDbSetting{setting, flags, value} =
+    case lookupSetting setting of
+      Nothing -> error "Impossible. Unknown setting was added to query packet"
+      Just MkSettingSerializer{serializer} ->
+        serialize rev setting
+        <> serialize rev flags
+        <> serializer rev value
 
 instance Serializable DbSettings where
   serialize rev (MkDbSettings setts) = do
     foldMap (serialize @DbSetting rev) setts
     <> serialize @ChString rev ""
   deserialize rev = do
-    setting <- deserialize @ChString rev
-    case setting of
-      "" -> pure $ MkDbSettings []
-      _ -> do
-        _ <- fail "Settings deserialization unsupported"
-        flags <- deserialize rev
-        value <- deserialize rev
-        addSetting MkDbSetting{..}
-          <$> deserialize rev
+    (MkChString setting) <- lookAhead (deserialize @ChString rev)
+    if BS.null setting
+      then skip 1 *> pure (MkDbSettings [])
+      else liftA2
+        addSetting
+        (deserialize @DbSetting rev)
+        (deserialize @DbSettings rev)
 
 data Flags = IMPORTANT | CUSTOM | TIER
 instance Serializable Flags where
@@ -50,3 +71,63 @@ instance Serializable Flags where
       0x02 -> pure CUSTOM
       0x0c -> pure TIER
       _ -> fail "Unknown flag code"
+
+
+data SettingSerializer  = 
+  MkSettingSerializer
+    { deserializer :: ProtocolRevision -> Get SettingType
+    , serializer   :: ProtocolRevision -> SettingType -> Builder
+    }
+
+lookupSetting :: ChString -> Maybe SettingSerializer
+lookupSetting name = lookup name (settingsMap @SupportedSettings)
+
+class SettingsMapBuilder (list :: [Type]) where
+  settingsMap :: SettingsMap
+
+instance SettingsMapBuilder '[] where settingsMap = []
+instance
+  (SettingsMapBuilder xs, IsSettingType settType, KnownSymbol name)
+  =>
+  SettingsMapBuilder (Setting name settType ': xs)
+  where
+  settingsMap =
+    let name = toChType (symbolVal @name Proxy)
+        desc = MkSettingSerializer
+          { deserializer = \rev -> toSettingType <$> deserialize @settType rev
+          , serializer = \rev -> serialize @settType rev . fromSettingType
+          }
+    in (name, desc) : settingsMap @xs
+
+type SettingsMap = [(ChString, SettingSerializer)]
+
+data SettingType where
+  SettingUInt64 :: UInt64 -> SettingType
+
+class
+  Serializable settType
+  =>
+  IsSettingType settType
+  where
+  toSettingType :: settType -> SettingType
+  fromSettingType :: SettingType -> settType
+
+instance IsSettingType UInt64 where
+  toSettingType uint64 = SettingUInt64 uint64
+  fromSettingType (SettingUInt64 uint64) = uint64
+
+mkSettingDesc :: forall name settType . (IsSettingType settType, KnownSymbol name) => (ChString, SettingSerializer)
+mkSettingDesc = 
+  let name = toChType (symbolVal @name Proxy)
+      desc = MkSettingSerializer
+        { deserializer = \rev -> toSettingType <$> deserialize @settType rev
+        , serializer = \rev -> serialize @settType rev . fromSettingType
+        }
+  in (name, desc)
+
+data Setting (a :: Symbol) (settType :: Type)
+
+type SupportedSettings = '[
+    Setting "max_threads_for_indexes" UInt64,
+    Setting "max_memory_usage" UInt64
+  ]
