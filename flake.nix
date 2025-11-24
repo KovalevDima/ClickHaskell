76159842c28b2c66bee4de185fcf8ab17082f3fd
Dmitry Kovalev <60290615+KovalevDima@users.noreply.github.com>
2025-11-24 06:08:39 +0300

    Update flake (#383)
    * Update flake

--- flake.nix
+++ flake.nix
@@ -28,8 +28,8 @@
         process-compose = {
           default = import ./contribution/localServer.nix {
             inherit inputs pkgs;
-            app = self'.apps.ghc984-server;
-            agent = self'.apps.ghc984-eventlog-agent;
+            app = self'.apps.ghc9103-server;
+            agent = self'.apps.ghc9103-eventlog-agent;
             docDirPath = self'.packages."documentation";
           };
         }
@@ -64,9 +64,9 @@
           //
           {
             default = pkgs.mkShell {
-              inputsFrom = [config.haskellProjects.ghc984.outputs.devShell];
+              inputsFrom = [config.haskellProjects.ghc9103.outputs.devShell];
               packages = with pkgs; with haskellPackages; with (self'.packages);
-                [clickhouse nodejs nil eventlog2html graphmod ghc984-html2hs markdown-unlit cloc];
+                [clickhouse nodejs nil eventlog2html graphmod ghc9103-html2hs markdown-unlit cloc];
             };
           };
         # Build documnetation
@@ -74,7 +74,7 @@
           "documentation" = import ./documentation/documentation.nix {inherit pkgs;};
           "ClickHaskell-dist" = import ./contribution/hackage.nix {
             inherit pkgs;
-            distPackage = self'.packages.ghc984-ClickHaskell;
+            distPackage = self'.packages.ghc9103-ClickHaskell;
           };
           "settsFile" = import ./contribution/genSettings.nix {
             inherit pkgs;
