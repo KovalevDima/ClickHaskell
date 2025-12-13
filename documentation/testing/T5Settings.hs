345ac520d082f0a55c5c5860aa20544efa7a4f8a
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-12-13 18:45:32 +0300

    Refactoring: t005-settings refactor query

--- documentation/testing/T5Settings.hs
+++ documentation/testing/T5Settings.hs
@@ -15,27 +15,20 @@ import GHC.Stack (HasCallStack)
 
 t5 :: HasCallStack => Connection -> IO ()
 t5 connection = do
-  let
-    sampleQuery =
-      fromGenerateRandom
-        @TestColumns
-        @TestData
-        (1, 10, 2)
-        1
-    addSampleSettings =
-      passSettings (
-        addSetting @"max_threads_for_indexes" 8 .
-        addSetting @"max_local_write_bandwidth" 4096 .
-        addSetting @"default_view_definer" "default" .
-        addSetting @"max_ast_depth" 1000 .
-        addSetting @"use_uncompressed_cache" True .
-        id
-      )
-    query = addSampleSettings sampleQuery
-
   _ <- select query connection pure
-
   putStrLn "t005: Ok"
+
+query :: Select TestColumns TestData
+query =
+  passSettings
+    ( id
+      . addSetting @"max_threads_for_indexes" 8
+      . addSetting @"max_local_write_bandwidth" 4096
+      . addSetting @"default_view_definer" "default"
+      . addSetting @"max_ast_depth" 1000
+      . addSetting @"use_uncompressed_cache" True
+    )
+    (fromGenerateRandom @TestColumns @TestData (1, 10, 2) 1)
 
 data TestData = MkTestData
   { testCol :: Int32
