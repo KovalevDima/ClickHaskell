29d0e9710ccc12dd283749ca1c9e739078c6201f
KovalevDima <onarainydayillwinanyway@gmail.com>
2026-02-01 20:21:23 +0300

    Actualize Settings.hs file

--- library/ClickHaskell/Protocol/Settings.hs
+++ library/ClickHaskell/Protocol/Settings.hs
@@ -146,7 +146,7 @@ instance IsSettingType Double where
   toSettingType float = SettingDouble float
   fromSettingType (SettingDouble float) = float
   fromSettingType _ = error "Impossible"
-  serializeSettingBinary rev = serialize @ChString rev . toChType . toQueryPart . fromSettingType @Float
+  serializeSettingBinary rev = serialize @ChString rev . toChType . toQueryPart . fromSettingType @Double
 
 instance IsSettingType UInt64 where
   toSettingType uint64 = SettingUInt64 uint64
