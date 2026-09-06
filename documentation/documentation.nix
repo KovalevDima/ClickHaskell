e8987cda5dacbd1c9fd12ab7b56e1a524db7bef2
KovalevDima <onarainydayillwinanyway@gmail.com>
2026-09-06 13:08:55 +0000

    Chore: npm update

--- documentation/documentation.nix
+++ documentation/documentation.nix
@@ -18,7 +18,11 @@ pkgs.stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pkgs.fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 4;
+<<<<<<< Updated upstream
     hash = "sha256-uv2/r1kBEYzSSsW6hZfm9xjgqvgiuhRklOxL77i2PPc=";
+=======
+    hash = "sha256-eVfcmXtV8uKgE1EgqUnfcsiVZrCY1C5764edTRBF28s=";
+>>>>>>> Stashed changes
     # hash = "${pkgs.lib.fakeHash}";
   };
 
